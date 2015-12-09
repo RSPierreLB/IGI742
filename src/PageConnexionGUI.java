927331b4dd0a39d7e75e1c5f844a637a01049bdc
Gauier <warnet.gautier@hotmail.fr>
2015-12-09 11:26:46 +0100

    Page

---
+++ src/PageConnexionGUI.java
@@ -0,0 +1,109 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener; 
+
+import javax.swing.*;
+
+public class PageConnexionGUI extends JFrame implements ActionListener{
+
+	private JPanel container = new JPanel();
+	
+	private JLabel lLogin = new JLabel("Login :");
+	private JLabel lPassword = new JLabel("Password :");
+	private JLabel lCreation = new JLabel("Pas de compte ?");
+	
+	protected JTextField tLogin = new JTextField(15);
+	protected JPasswordField tPassword = new JPasswordField(15);
+	
+	private JButton bouton_annuler = new JButton("Annuler");
+	private JButton bouton_connexion = new JButton("Connexion");
+	private JButton bouton_creer_compte = new JButton("Creer un compte");
+	
+	
+	// Constructor
+
+	public PageConnexionGUI() {
+		this.setSize(250, 200);  //size de la fenetre
+		this.setTitle("Connexion"); //titre de la fenetre
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setAlwaysOnTop(true);
+		this.AjoutElements(); //ajouter les elements qui sont dans la fonction
+		this.setContentPane(container);
+		this.setVisible(true);
+	}
+
+	
+	// Methods
+	
+	private void AjoutElements() {
+		
+		container.setBackground(Color.LIGHT_GRAY);
+	    container.setLayout(new GridLayout(4, 2, 5, 5));
+	    
+		lLogin.setHorizontalAlignment(SwingConstants.CENTER);
+		container.add(lLogin);
+		container.add(tLogin);
+		
+		lPassword.setHorizontalAlignment(SwingConstants.CENTER);
+		container.add(lPassword);
+		container.add(tPassword);
+		
+		bouton_annuler.addActionListener(this);
+		bouton_annuler.setActionCommand("Annuler");
+		container.add(bouton_annuler);  
+		
+	    bouton_connexion.addActionListener(new bouton_connexionListener());
+	    bouton_connexion.setActionCommand("Connexion");
+		container.add(bouton_connexion);
+		
+		
+		lCreation.setHorizontalAlignment(SwingConstants.CENTER);
+		container.add(lCreation);
+		bouton_creer_compte.addActionListener(new bouton_creer_compteListener());
+		bouton_creer_compte.setActionCommand("Creer_compte");
+		container.add(bouton_creer_compte);
+		
+	}
+	
+	//Classes ecoutant les Boutons	  
+	public class bouton_annulerListener implements ActionListener { 		// bouton_annuler bouton annuler
+		public void actionPerformed(ActionEvent e1) {
+			if((JButton)e1.getSource()==bouton_annuler) {
+				setVisible(false);
+			}
+		}
+	}
+	  
+	public class bouton_connexionListener implements ActionListener {			// bouton_connexion bouton connexion
+		public void actionPerformed(ActionEvent e2) {
+			if((JButton)e2.getSource()==bouton_connexion) {
+				
+			}
+		}  
+	}
+	
+	public class bouton_creer_compteListener implements ActionListener {			// bouton_creer_compte bouton creation
+		public void actionPerformed(ActionEvent e3) {
+			if((JButton)e3.getSource()==bouton_creer_compte) {
+				
+			}
+		}  
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		if(event.getActionCommand().equals("Annuler")){
+			
+		}
+		if(event.getActionCommand().equals("Connexion")){
+			
+		}
+		if(event.getActionCommand().equals("Creer_compte")){
+			PageInscriptionGUI pi = new PageInscriptionGUI();
+		}
+	}
+	
+}
---
+++ src/PageNonConnecterGUI.java
@@ -0,0 +1,118 @@
+
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowListener;
+
+import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+
+
+public class PageNonConnecterGUI extends JFrame implements ActionListener {
+	
+	private JPanel container = new JPanel();
+
+	private JPanel panBouton = new JPanel();
+	private JButton bouton_connexion = new JButton("Connexion");
+	private JButton bouton_recherche = new JButton("Recherche");
+	
+	private JTextField tRecherche = new JTextField(30); //champ de recherche
+	private String recherche;	//variable récupération recherche
+	
+	private JPanel panListeAnnonces = new JPanel();
+	private DefaultListModel liste_des_annonces = new DefaultListModel();
+	private JList listeAnnonces = new JList(liste_des_annonces);
+	
+	
+	public PageNonConnecterGUI() {
+		
+		this.setSize(540, 500); 												//size de la fenetre
+		this.setTitle("Application de partage sur le campus"); 					//titre de la fenetre
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLocationRelativeTo(null);
+		this.setResizable(false);
+		this.setAlwaysOnTop(true);
+		this.AjoutElements(); 													//ajouter les elements qui sont dans la fonction
+		this.setContentPane(container);
+		this.setVisible(true);
+	}
+
+	private void AjoutElements() {
+		
+		// Creation des boutons
+		bouton_recherche.setPreferredSize(new Dimension(200, 40)); 							//dimension du bouton1
+		bouton_recherche.addActionListener(this);
+		bouton_recherche.setActionCommand("Rechercher");
+		
+		bouton_connexion.setPreferredSize(new Dimension(200, 40)); 							//dimension du bouton2    
+		bouton_connexion.addActionListener(this);
+		bouton_connexion.setActionCommand("Connexion");
+		
+
+		panBouton.setLayout(new GridLayout(1, 2));
+		panBouton.setBorder(new LineBorder(new Color(0, 0, 0)));
+		panBouton.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+		panBouton.setPreferredSize(new Dimension(520,50)); 						//size du panneau
+		panBouton.setBackground(Color.RED); 									//couleur fond panneau bouton
+		panBouton.add(bouton_recherche);
+		panBouton.add(tRecherche); //Champ de recherche
+		panBouton.add(bouton_connexion);
+	    
+		
+		
+		// Affichage des annonces
+	    listeAnnonces.setBackground(Color.WHITE);								//couleur du fond de la liste
+	    listeAnnonces.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);	//selection que d'une seule ligne possible
+	    listeAnnonces.setSize(400, 300); 										//size de la liste
+	    listeAnnonces.addListSelectionListener(new listeAnnonceListener());
+	    listeAnnonces.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+		
+	    panListeAnnonces.setBorder(new LineBorder(new Color(0, 0, 0)));
+	    panListeAnnonces.setPreferredSize(new Dimension(520,410)); 				//size du panneau
+	    panListeAnnonces.add(listeAnnonces);
+	    
+	   		
+
+		//ajout tous les elements dans la fenetre
+		//container.setLayout(new GridLayout(2, 1));
+		container.add(panBouton);
+		container.add(panListeAnnonces);
+		
+	}
+	
+	
+	// Classes Listener
+	public class bouton_rechercheListener implements ActionListener { 						//Bouton recherche
+		public void actionPerformed(ActionEvent e1) {
+
+		}
+	}
+	
+	public class bouton_connexionListener implements ActionListener { 						//Bouton connexion
+		public void actionPerformed(ActionEvent e2) {
+
+		}
+	}
+
+	public class listeAnnonceListener implements ListSelectionListener {		//Liste des annonces
+		public void valueChanged(ListSelectionEvent l1) {
+			
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent event) {
+		// TODO Auto-generated method stub
+		if(event.getActionCommand().equals("bouton_connexion")){
+			PageConnexionGUI pc = new PageConnexionGUI(); 
+		}
+		
+		if(event.getActionCommand().equals("bouton_recherche")){
+			recherche = tRecherche.getText();//récupérer les valeurs du champ 
+		}
+	}
+
+}
