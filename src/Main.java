ebfca2944bec94a995d8c9b8d8e6f72c88f4bb67
alexxxlamy <alexandre.lamy@hotmail.fr>
2015-12-28 16:48:04 +0100

    alex

--- src/Main.java
+++ src/Main.java
@@ -3,9 +3,11 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Administrateur admin= Administrateur.getInstance();
 		
-
+		PageNonConnecterGUI pnc = new PageNonConnecterGUI();
+		
+		
+		Administrateur admin= Administrateur.getInstance();
 		
 		Utilisateur u1= new Utilisateur("Lucas","Warnet");
 		
